5d2daf5470fa58e15d0d874943520a6e96d96911
Skylar Wurster <swwurster@gmail.com>
2019-01-14 17:49:12 -0500

    Framework for 4 total visualizations

--- Assignment 1/Assets/Scripts/DataVisualizer.cs
+++ Assignment 1/Assets/Scripts/DataVisualizer.cs
@@ -18,28 +18,47 @@ public class DataVisualizer : MonoBehaviour
         data = Dataset1Reader.ReadFile(fileLocation);
         ParticleSystem.MainModule mainModule = vizGameObject.GetComponent<ParticleSystem>().main;
         mainModule.maxParticles = data.Count * data[0].Count;
-        SetParticlePoints();
-        Visualization1();
-    }
-    
+        InstantiateParticles();        
+    }    
 
-    void SetParticlePoints()
+    void InstantiateParticles()
     {
         particles = new ParticleSystem.Particle[data.Count * data[0].Count];
-
         for(int i = 0; i < data.Count * data[0].Count; i++)
         {
             particles[i].position = new Vector3(xSpacing * (i % data[0].Count), 0, zSpacing * ((int)(i / data[0].Count)));
             particles[i].startSize = 1;
             particles[i].startColor = Color.black;
         }
-
-        vizGameObject.GetComponent<ParticleSystem>().SetParticles(particles, data[0].Count * data.Count);
-        
+        vizGameObject.GetComponent<ParticleSystem>().SetParticles(particles, data[0].Count * data.Count);       
     }
 
-    void Visualization1()
+    public void StartVisualization1()
     {
+        this.StopAllCoroutines();
+        StartCoroutine(Visualization1Pretty(2));
+    }
+
+    IEnumerator Visualization1Pretty(float seconds)
+    {
+        float startTime = Time.time;
+        while(Time.time - startTime < seconds)
+        {
+            for (int i = 0; i < data.Count * data[0].Count; i++)
+            {
+                float dataVal = data[(int)(i / data[0].Count)][i % data[0].Count];
+                particles[i].position = Vector3.Lerp(particles[i].position, 
+                    new Vector3(particles[i].position.x, dataVal * maxHeight, particles[i].position.z),
+                    0.25f);
+                particles[i].startColor = Color.Lerp(particles[i].startColor, 
+                    Color.Lerp(Color.black, Color.white, dataVal), 
+                    0.25f);
+            }
+
+            vizGameObject.GetComponent<ParticleSystem>().SetParticles(particles, data[0].Count * data.Count);
+
+            yield return null;
+        }
         for (int i = 0; i < data.Count * data[0].Count; i++)
         {
             float dataVal = data[(int)(i / data[0].Count)][i % data[0].Count];
@@ -48,6 +67,22 @@ public class DataVisualizer : MonoBehaviour
         }
 
         vizGameObject.GetComponent<ParticleSystem>().SetParticles(particles, data[0].Count * data.Count);
+    }
+
+    public void StartVisualization2()
+    {
 
     }
+
+    public void StartVisualization3()
+    {
+
+    }
+
+    public void StartVisualization4()
+    {
+
+    }
+
 }
+
