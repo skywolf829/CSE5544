c0d3131749b597c57be74be1b8acdfd313e01840
Skylar Wurster <swwurster@gmail.com>
2019-01-14 15:17:48 -0500

    Imports CSV data and uses that as input to a particle system for visualization.

---
+++ Assignment 1/Assets/Scripts/DataVisualizer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataVisualizer : MonoBehaviour
+{
+    public string fileLocation = "Assets/Data/dataset1.txt";
+    public float xSpacing, zSpacing;
+    public float maxHeight;
+    public GameObject vizGameObject;
+
+    List<List<float>> data;
+
+    ParticleSystem.Particle[] particles;
+
+    void Start()
+    {
+        data = Dataset1Reader.ReadFile(fileLocation);
+        ParticleSystem.MainModule mainModule = vizGameObject.GetComponent<ParticleSystem>().main;
+        mainModule.maxParticles = data.Count * data[0].Count;
+        SetParticlePoints();
+        Visualization1();
+    }
+    
+
+    void SetParticlePoints()
+    {
+        particles = new ParticleSystem.Particle[data.Count * data[0].Count];
+
+        for(int i = 0; i < data.Count * data[0].Count; i++)
+        {
+            particles[i].position = new Vector3(xSpacing * (i % data[0].Count), 0, zSpacing * ((int)(i / data[0].Count)));
+            particles[i].startSize = 1;
+            particles[i].startColor = Color.black;
+        }
+
+        vizGameObject.GetComponent<ParticleSystem>().SetParticles(particles, data[0].Count * data.Count);
+        
+    }
+
+    void Visualization1()
+    {
+        for (int i = 0; i < data.Count * data[0].Count; i++)
+        {
+            float dataVal = data[(int)(i / data[0].Count)][i % data[0].Count];
+            particles[i].position = new Vector3(particles[i].position.x, dataVal * maxHeight, particles[i].position.z);
+            particles[i].startColor = Color.Lerp(Color.black, Color.white, dataVal);
+        }
+
+        vizGameObject.GetComponent<ParticleSystem>().SetParticles(particles, data[0].Count * data.Count);
+
+    }
+}
---
+++ Assignment 1/Assets/Scripts/Dataset1Reader.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Dataset1Reader 
+{
+   public static List<List<float>> ReadFile(string file)
+    {
+        List<List<float>> values = new List<List<float>>();
+
+        string[] lines = System.IO.File.ReadAllLines(file);
+
+        foreach (string line in lines)
+        {
+            char[] lineArray = line.ToCharArray();
+            string currentValue = "";
+            int currentPosition = 0;
+            List<float> currentValues = new List<float>();
+
+            while(currentPosition < line.Length)
+            {
+                if (lineArray[currentPosition] == ' ')
+                {
+                    currentValues.Add(float.Parse(currentValue));
+                    currentValue = "";
+                }
+                else
+                {
+                    currentValue = currentValue + lineArray[currentPosition];
+                }
+                currentPosition++;
+            }
+
+            if(currentValue != "")
+            {
+                currentValues.Add(float.Parse(currentValue));
+                currentValue = "";
+            }
+
+            values.Add(currentValues);
+        }
+
+        return values;
+    }
+}
